4b0f635e2f7fc59629928e2ecbc7d1728fb46736
marimelody11 <marimelody11@gmail.com>
2014-05-12 21:14:54 +0900

    update manifest.json

--- js/background.js
+++
@@ -1,9 +0,0 @@
-// アイコンクリック時のイベントを指定する
-chrome.browserAction.onClicked.addListener(function () {
-  // chrome.browserAction.setPopupに渡すパラメータを作成する
-  var detail = {
-    popup: 'popup.html'
-  };
-  // ポップアップを開く
-  chrome.browserAction.setPopup(detail);
-});
